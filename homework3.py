ecb34e1514ebab427ab9cb86aa68b9b7fc2712fe
kwonsean <gownn12@naver.com>
2021-08-03 21:38:57 +0900

    Feat: Add input function to receive information
    Add two input functions to receive user'interest, file name

--- homework3.py
+++ homework3.py
@@ -1,1 +1,9 @@
 from NaverNewsCrawler import NaverNewsCrawler
+
+user_input = input('키워드를 입력하세요: ')
+
+crawler = NaverNewsCrawler(user_input)
+
+user_interset = input('엑셀 파일명을 입력하세요: ')
+
+crawler.get_news(user_interset)
