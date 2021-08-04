f5ab5ac4f531e8b2729ac6208a58a4efd5a7db88
kwonsean <gownn12@naver.com>
2021-08-05 01:07:37 +0900

    feat: Change for sentence to filter method
    보내주신 링크를 참고하여 filter함수를 활용하여 코드를 수정하였습니다.

--- main.js
+++ main.js
@@ -12,9 +12,11 @@ const data = [
     { name: '껌', price: 500 }
 ];
 
+
 // 사용자 입력 받기
 const line = prompt('최대 금액을 입력해주세요.');
 const amount = Number(line);
+
 
 // 주어진 금액으로 살 수 있는 가장 비싼 상품을 구함
 const item = getItemByAmount(data, amount);
@@ -36,25 +38,25 @@ function getItemByAmount(data, amount){
 
     // 먼저 입력값이 양수인지 확인 
     if(amount > 0){
-        for(let i = 0; i<data.length; i++){
-            const PRICE = data[i].price;
-            // 구매 가능한지 확인 
-            if(amount >= PRICE){
-                change = amount - PRICE;
-                //최소 잔돈인지 확인 
+        const result = data.filter(function (item, index, self){
+            const PRICE = item.price;
+            if (amount >= PRICE){
+                change = amount - PRICE
                 if(smallestChange > change){
-                    smallestChange = change;
-                    smallestChangeIndex = i;
-                } else{
-                    continue;
+                    smallestChange = change
+                    smallestChangeIndex = index
                 }
-                
-            } else{
-            continue;
-            }
+                //filter함수는 true인 값들을 배열속으로 모두 return하기 때문에 우선 최소잔돈이 초기화 될때마다 result배열에 return시행
+                if (index == smallestChangeIndex ){
+                    return self[smallestChangeIndex]
+                }
         }
+        });
+        // result에는 최소 잔돈이 초기화 될때마다 객체의 형태로 배열에 추가가 되었으니 가장 마지막에 추가된 요소가 가장 작은 잔돈을 발생시키는 객체이다
+        endGame = result.pop()
+        return endGame
+     
     } else{
         return null;
     }
-   return data[smallestChangeIndex];
 };
